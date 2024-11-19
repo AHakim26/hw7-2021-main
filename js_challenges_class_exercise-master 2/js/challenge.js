cab4976dd35dde08957f694d7ec5f78ba7e5dd0d
Ayesha Hakim <ayeshaha@umich.edu>
2024-11-19 15:37:18 -0500

    Delete js_challenges_class_exercise-master 2 directory

--- js_challenges_class_exercise-master 2/js/challenge.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#subscribe").addEventListener("click",function(){
-    console.log("thisworked");
-    if (this.checked){
-        document.querySelector("#emailDiv").style.display="block"
-    }
-    else{
-        document.querySelector("#emailDiv").style.display="none"
-    }});
--- js_challenges_class_exercise-master 2/js/challenge2.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#sameAddress").addEventListener("click",function(){
-    console.log("thisworked");
-    if (this.checked){
-        document.querySelector("#home").value=document.querySelector("#billing").value
-    }
-    else{
-        document.querySelector("#home").style.display="none"
-    }});
